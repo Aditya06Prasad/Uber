46baf9fbf8939ce360e1d6ac0fa28e1392177e4e
Aditya Prasad <prasadaditya389@gmail.com>
2026-02-19 12:12:22 +0530

    Done with home page

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,82 +1,79 @@
 const captainModel = require('../Models/captain.model');
 const captainService = require('../services/captain.service');
 const { validationResult } = require('express-validator');
-const blackListToken = require('../Models/blackListToken.model');
 const blackListTokenModel = require('../Models/blackListToken.model');
 
-module.exports.registerCaptain= async (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
+// Register Captain
+module.exports.registerCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-        const { fullname, email, password, vehicle } = req.body;
+    const { fullname, email, password, vehicle } = req.body;
 
-        const isCaptainAlreadyExist = await captainModel.findOne({ email });
+    const isCaptainAlreadyExist = await captainModel.findOne({ email });
 
-        if (isCaptainAlreadyExist) {
-            return res.status(400).json({ message: 'Captain already exists' });
-        }
+    if (isCaptainAlreadyExist) {
+        return res.status(400).json({ message: 'Captain already exists' });
+    }
 
-        const hashedPassword = await captainModel.hashPassword(password);
+    const hashedPassword = await captainModel.hashPassword(password);
 
     const captain = await captainService.createCaptain({
-    fullname: {
-        firstname: fullname.firstname,
-        lastname: fullname.lastname
-    },
-    email,
-    password: hashedPassword,
-    vehicle: {
-        colour: vehicle.colour,
-        plate: vehicle.plate,
-        capacity: vehicle.capacity,
-        vehicleType: vehicle.vehicleType
-        }
+        fullname,
+        email,
+        password: hashedPassword,
+        vehicle
     });
 
+    const token = captain.generateAuthToken();
+    res.status(201).json({ token, captain });
+};
 
-        const token = captain.generateAuthToken();
-        res.status(201).json({ token, captain });
+
+// Login Captain
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 
-    //Login
-    module.exports.loginCaptain = async(req, res, next) => {
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({ errors: errors.array()});
-        }
-        const { email, password} = req.body;
-        const user = await captainModel.findOne({ email }).select('+password');
-    
-        if(!user){
-            return res.status(401).json({ message: 'Invalid email or password' })
-        }
-    
-        const isMatch = await captain.comparePassword(password);
-    
-        if(!isMatch){
-            return res.status(401).json({ message: 'Invalid email or Passwprd' });
-        }
-    
-        const token = captain.generateAuthToken();
-    
-        res.status(200).json({ token, user });
+    const { email, password } = req.body;
+
+    const captain = await captainModel.findOne({ email }).select('+password');
+
+    if (!captain) {
+        return res.status(401).json({ message: 'Invalid email or password' });
     }
 
-    // User Profile
-    module.exports.getCaptainProfile =  async (req, res) => {
-        res.status(200).json(req.user);
-    } 
-    
-    //logout User
-    
-    module.exports.logoutCaptain = async (req, res, next)=> {0
-        res.clearCookie('token');
-        const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
-        
-        await blackListTokenModel.create({token})
-        res.status(200).json({ message: 'Logged Out' });
+    const isMatch = await captain.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
     }
-    
- 
+
+    const token = captain.generateAuthToken();
+    res.cookie('token', token);
+
+    res.status(200).json({ token, captain });
+};
+
+
+// Captain Profile
+module.exports.getCaptainProfile = async (req, res) => {
+    res.status(200).json(req.user);
+};
+
+
+// Logout Captain
+module.exports.logoutCaptain = async (req, res, next) => {
+    res.clearCookie('token');
+
+    const token =
+        req.cookies.token || req.headers.authorization.split(' ')[1];
+
+    await blackListTokenModel.create({ token });
+
+    res.status(200).json({ message: 'Logged Out' });
+};
--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -3,6 +3,7 @@ const captainModel = require('../Models/captain.model')
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const blackListToken = require('../Models/blackListToken.model');
+const blackListTokenModel = require('../Models/blackListToken.model');
 
 module.exports.authUser = async (req, res, next) => {
     const token = req.cookies.token || req.headers.authorization?.split(' ')[ 1 ];
@@ -12,7 +13,7 @@ module.exports.authUser = async (req, res, next) => {
 
     try {
 
-        const isBlacklisted = await blackListToken.findOne({ token });
+        const isBlacklisted = await blackListTokenModel.findOne({ token });
         if (isBlacklisted) {
             return res.status(401).json({ message: 'Token is blacklisted' });
         }
@@ -30,21 +31,32 @@ module.exports.authUser = async (req, res, next) => {
 }
 
 module.exports.authCaptain = async (req, res, next) => {
-    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
-
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-
     try {
+        const token =
+            req.cookies.token ||
+            (req.headers.authorization &&
+                req.headers.authorization.split(' ')[1]);
+
+        if (!token) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
+        const isBlacklisted = await blackListTokenModel.findOne({ token });
+        if (isBlacklisted) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
+
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         const captain = await captainModel.findById(decoded._id);
 
-        req.captain = captain;
-        return next();
+        if (!captain) {
+            return res.status(401).json({ message: 'Unauthorized' });
+        }
 
+        req.user = captain;
+        next();
     } catch (err) {
-        return res.status(401).json({ message: 'Unauthorized Captain' });
+        return res.status(401).json({ message: 'Unauthorized' });
     }
 };
